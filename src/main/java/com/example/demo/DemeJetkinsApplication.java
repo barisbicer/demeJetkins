e1317bf6557d285662cad8317aa5ba74b03675fb
barisbicer <baris_bicer@hotmail.com>
2020-10-01 16:16:39 +0300

    first commit

---
+++ src/main/java/com/example/demo/DemeJetkinsApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class DemeJetkinsApplication {
+	
+	public static Logger logger =org.slf4j.LoggerFactory.getLogger(DemeJetkinsApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		
+		logger.info("Application started.......");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application executed.......");
+		SpringApplication.run(DemeJetkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/DemeJetkinsApplicationTests.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class DemeJetkinsApplicationTests {
+
+	
+	public static Logger logger =org.slf4j.LoggerFactory.getLogger(DemeJetkinsApplicationTests.class);
+	
+	
+	@Test
+	void contextLoads() {
+	
+		logger.info("Test case started.......");
+		
+		
+		
+	}
+
+}
